2ddc46203ae06f6aebb741d4ae40701390940b91
李木 <limuxljy@dingding.com>
2017-11-01 15:12:25 +0800

    增加文章详情页

--- routes/article.js
+++ routes/article.js
@@ -2,8 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/:id', function (req, res) {
-  console.log(req.id)
-  res.render('index', { title: '关于一部电影的全部' });
+  res.render('article', { title: '你眼中的世界' });
 })
 
 module.exports = router
